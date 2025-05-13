e2ecd5377876767e03ff6339237d53c0c3ac7089
leowader <waderhe@gmail.com>
2025-05-13 17:01:08 -0500

    terminado

--- src/main/java/com/patrones/server_rest/domain/adaptador/IFactura.java
+++ src/main/java/com/patrones/server_rest/domain/adaptador/IFactura.java
@@ -6,6 +6,6 @@ import com.patrones.server_rest.domain.Factura;
 public interface IFactura {
 
     FacturaSoap convertirFacturaJsonToXml (Factura facturaJson );
-    com.patrones.server_rest.domain.adaptador.FacturaJson convertirFacturaXmlToJson(String facturaXml);
+    FacturaJson convertirFacturaXmlToJson(String facturaXml);
 
 }
